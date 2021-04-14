2f6261e74e2720829fb3daae2b8ead808deefba5
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-14 20:32:59 +0100

    Add project files.

---
+++ Main_/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Main_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"{args.Length} command line arguments were specified:");
+            foreach (string arg in args)
+                WriteLine(arg);
+            ReadKey();
+        }
+    }
+}
